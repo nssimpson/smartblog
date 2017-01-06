acb5eea766e2ec10a3d5d09c04a8924338148a5c
nssimpson <nick.simpson@devilprinters.co.uk>
2017-01-06 02:22:58 +0000

    Rewrite Update
    If SmartBlogLink.php is updated then this will need to be updated too.

--- htupdate.php
+++ htupdate.php
@@ -2,6 +2,6 @@
 
 $htupdate = '# Images Blog
 
-RewriteRule ^blog/([0-9]+)(\-[\.*_a-zA-Z0-9-]*)(-[0-9]+)?/.+\.jpg$ %{ENV:REWRITEBASE}modules/smartblog/images/$1$2$3.jpg [L]
-RewriteRule ^blog/([a-zA-Z_-]+)(-[0-9]+)?/.+\.jpg$ %{ENV:REWRITEBASE}modules/smartblog/images/$1$2.jpg [L]';
+RewriteRule ^smartblogredirect/([0-9]+)(\-[\.*_a-zA-Z0-9-]*)(-[0-9]+)?/.+\.jpg$ %{ENV:REWRITEBASE}modules/smartblog/images/$1$2$3.jpg [L]
+RewriteRule ^smartblogredirect/([a-zA-Z_-]+)(-[0-9]+)?/.+\.jpg$ %{ENV:REWRITEBASE}modules/smartblog/images/$1$2.jpg [L]';
 
